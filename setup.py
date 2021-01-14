bb29e5c4749d300cd419c034f6a635a389cb8d76
Martin Stefanik <stefanik.mar@gmail.com>
2021-01-15 00:23:11 +0100

    Add a setup file for setuptools

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (c) 2021 Martin Stefanik
+"""
+
+from setuptools import setup, find_packages
+from daddy import VERSION
+
+
+with open('README.md') as f:
+    README = f.read()
+
+with open('requirements.txt', 'r') as f:
+    REQUIRES = f.read().splitlines()
+
+setup(
+    name='daddy',
+    author='Martin Stefanik',
+    author_email='stefanik.mar@gmail.com',
+    maintainer='Martin Stefanik',
+    maintainer_email='stefanik.mar@gmail.com',
+    version=VERSION,
+    description='CLI tool for verification of domain name availability.',
+    long_description=README,
+    url='https://github.com/martinstefanik/daddy',
+    license='MIT',
+    py_modules=['daddy'],
+    packages=find_packages(),
+    python_requires='>=3.8',
+    install_requires=REQUIRES,
+    entry_points="""
+        [console_scripts]
+        daddy=daddy:daddy
+    """
+)
