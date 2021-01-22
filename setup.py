7f21c369e49c337f57ef721f9c06fffd311f3156
Martin Stefanik <stefanik.mar@gmail.com>
2021-01-22 20:24:33 +0100

    Move settings to setup.cfg

--- setup.py
+++ setup.py
@@ -1,36 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Copyright (c) 2021 Martin Stefanik
-"""
+"""Copyright (c) 2021 Martin Stefanik"""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-from daddy import VERSION
-
-with open('README.md') as f:
-    README = f.read()
-
-with open('requirements.txt', 'r') as f:
-    REQUIRES = f.read().splitlines()
-
-setup(
-    name='daddy',
-    author='Martin Stefanik',
-    author_email='stefanik.mar@gmail.com',
-    maintainer='Martin Stefanik',
-    maintainer_email='stefanik.mar@gmail.com',
-    version=VERSION,
-    description='CLI tool for verification of domain name availability.',
-    long_description=README,
-    url='https://github.com/martinstefanik/daddy',
-    license='MIT',
-    py_modules=['daddy'],
-    packages=find_packages(),
-    python_requires='>=3.6',
-    install_requires=REQUIRES,
-    entry_points="""
-        [console_scripts]
-        daddy=daddy:daddy
-    """
-)
+setup()
